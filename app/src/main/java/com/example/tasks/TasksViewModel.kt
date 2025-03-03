6fba300b23564483a771517e3ebad8da478c55e8
meeten <153901120+meeten@users.noreply.github.com>
2025-03-03 08:56:07 +0200

    Создан класс TakssViewModel

---
+++ app/src/main/java/com/example/tasks/TasksViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.tasks
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class TasksViewModel(private val dao:TaskDao): ViewModel() {
+    var newTaskName = ""
+
+    fun addTask(){
+        viewModelScope.launch {
+            val task = Task()
+            task.taskName = newTaskName
+            dao.insert(task)
+        }
+    }
+}
