ee1d839464067d9020b999f2dfe3ad656cd9d2ba
meeten <153901120+meeten@users.noreply.github.com>
2025-03-21 21:12:15 +0200

    Импорт плагина Safe Args Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
+}
+
+
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
