e1417c41f54f889ddf6b4de7e0d8ae36c6e19005
meeten <153901120+meeten@users.noreply.github.com>
2025-03-24 10:58:43 +0200

    Логика EditTaskFragment

--- app/src/main/java/com/example/tasks/EditTaskFragment.kt
+++ app/src/main/java/com/example/tasks/EditTaskFragment.kt
@@ -5,14 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.example.tasks.databinding.FragmentEditTaskBinding
 
 class EditTaskFragment : Fragment() {
+    private var _binding: FragmentEditTaskBinding? = null
+    private val binding get() = _binding!!
 
+    private lateinit var editTaskViewModelFactory: EditTaskViewModelFactory
+    private lateinit var editTaskViewModel: EditTaskViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_edit_task, container, false)
+    ): View {
+        _binding = FragmentEditTaskBinding.inflate(inflater)
+        val view = binding.root
+        val application = requireNotNull(this.activity).application
+        val taskDao = TaskDatabase.getInstance(application).taskDao
+        val taskId = EditTaskFragmentArgs.fromBundle(requireArguments()).taskId
+        editTaskViewModelFactory = EditTaskViewModelFactory(taskId, taskDao)
+        editTaskViewModel =
+            ViewModelProvider(this, editTaskViewModelFactory)[EditTaskViewModel::class.java]
+        binding.editTaskViewModel = editTaskViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        editTaskViewModel.navigatedToList.observe(viewLifecycleOwner) { navigate ->
+            if (navigate) {
+                view.findNavController().navigate(R.id.action_editTaskFragment_to_tasksFragment)
+                editTaskViewModel.onListNavigated()
+            }
+        }
+
+        return view
+    }
+
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 
 }
